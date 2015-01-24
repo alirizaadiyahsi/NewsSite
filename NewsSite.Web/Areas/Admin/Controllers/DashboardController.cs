e3c8b2353dd89a7330ac1c6cb69a8d24954e6726
alirizaadiyahsi <alirizaadiyahsi@gmail.com>
2015-01-24 21:24:16 +0200

    account views

---
+++ NewsSite.Web/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,20 @@
+﻿using NewsSite.Data.UnitOfWork;
+using NewsSite.Web.Framework.Controllers;
+using System.Web.Mvc;
+
+namespace NewsSite.Web.Areas.Admin.Controllers
+{
+    public class DashboardController : AdminController
+    {
+        public DashboardController(IUnitOfWork uow)
+            : base(uow)
+        { 
+        
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
