e45cd56c59e8a911271fcb540b675a45a89b7c97
alirizaadiyahsi <alirizaadiyahsi@gmail.com>
2015-02-19 00:19:40 +0200

    sync all files

---
+++ NewsSite.Web/Controllers/PostController.cs
@@ -0,0 +1,41 @@
+﻿using NewsSite.Data.UnitOfWork;
+using NewsSite.Service.CategoryServices;
+using NewsSite.Service.PostServices;
+using NewsSite.Web.Framework.Controllers;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace NewsSite.Web.Controllers
+{
+    public class PostController : PublicController
+    {
+        private readonly IPostService _postService;
+        private readonly ICategoryService _categoryService;
+
+        public PostController(IUnitOfWork uow, IPostService postService, ICategoryService categoryService)
+            : base(uow)
+        {
+            _categoryService = categoryService;
+            _postService = postService;
+        }
+
+        public ActionResult CategoryDetail(int id)
+        {
+            var category = _categoryService.Find(id);
+
+            return View(category);
+        }
+
+        public ActionResult PostDetail(int id)
+        {
+            var post = _postService.Find(id);
+
+            post.ReadCount++;
+            _postService.Update(post);
+            _uow.SaveChanges();
+
+            return View(post);
+        }
+    }
+}
---
+++ NewsSite.Web/Views/PostController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace NewsSite.Web.Views
+{
+    public class PostController : Controller
+    {
+        public ActionResult Detail(int postId)
+        {
+            return View();
+        }
+
+        public ActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
